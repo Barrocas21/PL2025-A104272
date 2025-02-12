b4308b37cebb9744c356148db6238e1795709573
Miguel Barrocas <a104272@alunos.uminho.pt>
2025-02-12 11:21:10 +0000

    .

---
+++ TPC1/tp1.py
@@ -0,0 +1,14 @@
+
+
+def somador_on_off (texto):
+    soma = 0 #Armazena a soma dos números
+    soma_atual = True #Verifica se devemos somar os números ou não
+    numero_atual = "" #Armazena o "número atual" que esta a ser lido mais soma
+
+    i = 0
+    while i<len(texto):
+        char = texto[i]
+
+        
+
+    
